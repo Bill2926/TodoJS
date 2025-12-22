92bef5580ee213aa7a467d7ae7ff1bb47eb2fbb6
Bill2926 <nguyenducmanh2926@gmail.com>
2025-12-22 22:47:38 +1030

    modal and eventlistener
    22/12/2025 - Glenelg, Adelaide
    - added Finish and Delete buttons
    - added modal to notify delete process
    - learnt about eventlistener in JS

--- script/current.js
+++ script/current.js
@@ -1,4 +1,7 @@
-let todo = JSON.parse(localStorage.getItem("todo"));
+let todo = JSON.parse(localStorage.getItem("todo"))
+const mainContainer = document.getElementById('mid')
+const modalContainer = document.getElementById('modal')
+const noModalBtn = document.getElementById('no')
 
 if (todo == null) {
     document.getElementById('mid').innerHTML = "There is currently now task. Please consider adding one!";
@@ -24,7 +27,6 @@ function createTaskBoxes() {
         priority_p.className = 'priority';
 
         const due_p = document.createElement('p');
-        // due_p.textContent = due;
         due_p.innerHTML = `Due: <span class='due_time'>${due}</span>`;
         due_p.className = 'due';
 
@@ -32,11 +34,43 @@ function createTaskBoxes() {
         created_at_p.innerHTML = `Created at: <span class="created_at">${created_at}</span>`;
         created_at_p.className = 'time_created';
 
+        const finished_btn = document.createElement('button');
+        finished_btn.innerHTML = 'Finish';
+        finished_btn.className = 'finished_btn';
+
+        const delete_btn = document.createElement('button');
+        delete_btn.innerHTML = 'Delete';
+        delete_btn.className = 'delete_btn';
+
         newDiv.appendChild(name_p);
         newDiv.appendChild(priority_p);
         newDiv.appendChild(due_p);
         newDiv.appendChild(created_at_p);
+        newDiv.appendChild(finished_btn);
+        newDiv.appendChild(delete_btn);
 
         document.getElementById('mid').appendChild(newDiv);
     };
 }
+
+mainContainer.addEventListener("click", (e) => {
+    // if (e.target.className == "delete_btn") {
+    //     modalContainer.style.display = "block";
+    // }
+
+    // this will handle the situation where class name looks like this
+    // <div class="delete_btn new old"></div>
+    if (e.target.classList.contains("delete_btn")) {
+        modalContainer.style.display = "block";
+    }
+})
+
+noModalBtn.onclick = function() {
+    modalContainer.style.display = "none";
+}
+
+modalContainer.addEventListener("click", (e) => {    
+    if (e.target == modalContainer) {
+        modalContainer.style.display = "none";
+    }
+})
